98da5aa79fdbab673146b756acc343d98dd6703f
vasanwalker <36722736+vasanwalker@users.noreply.github.com>
2018-02-23 11:48:03 +0530

    Create primenumber2inter.py

---
+++ beginner level/primenumber2inter.py
@@ -0,0 +1,17 @@
+low = 900
+upp = 1000
+
+# uncomment the following lines to take input from the user
+#lower = int(input("Enter lower range: "))
+#upper = int(input("Enter upper range: "))
+
+print("Prime numbers between",low,"and",upp,"are:")
+
+for num in range(low,upp + 1):
+   # prime numbers are greater than 1
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
