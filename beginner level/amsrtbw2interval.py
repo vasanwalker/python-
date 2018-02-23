e16f26cf6ed8ed3da3e9402c609b151af9e64cd5
vasanwalker <36722736+vasanwalker@users.noreply.github.com>
2018-02-23 11:56:11 +0530

    Create amsrtbw2interval.py

---
+++ beginner level/amsrtbw2interval.py
@@ -0,0 +1,20 @@
+low = 150
+upp = 160
+
+for num in range(low, upp + 1):
+
+   # order of number
+   order = len(str(num))
+    
+   # initialize sum
+   sum = 0
+
+   # find the sum of the cube of each digit
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)
